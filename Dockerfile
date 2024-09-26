194b60f74a487835145e808d79ee2f343fec732c
WiGHT <1572758387@qq.com>
2024-09-26 17:04:45 +0100

    feat: initial file directory from https://github.com/mglienecke/IlpDataObjects/tree/main

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:17
+COPY ./target /tmp
+WORKDIR /tmp
+ENTRYPOINT ["java","App"]
+# Please note that the THIRD-PARTY-LICENSE could be out of date if the base image has been updated recently.
+# The Corretto team will update this file but you may see a few days' delay.
---
+++ src/main/java/uk/ac/ed/inf/constant/OrderValidationCode.java
@@ -0,0 +1,56 @@
+package uk.ac.ed.inf.constant;
+
+/**
+ * error reason codes for an invalid order
+ */
+public enum OrderValidationCode {
+    /**
+     * the reason code is undefined
+     */
+    UNDEFINED,
+
+    /**
+     * no error present
+     */
+    NO_ERROR,
+
+    /**
+     * the card number is incorrect
+     */
+    CARD_NUMBER_INVALID,
+
+    /**
+     * expiry date problem
+     */
+    EXPIRY_DATE_INVALID,
+
+    /**
+     * CVC is wrong
+     */
+    CVV_INVALID,
+
+    /**
+     * order total is incorrect
+     */
+    TOTAL_INCORRECT,
+
+    /**
+     * a pizza in the order is undefined
+     */
+    PIZZA_NOT_DEFINED,
+
+    /**
+     * too many pizzas ordered
+     */
+    MAX_PIZZA_COUNT_EXCEEDED,
+
+    /**
+     * pizzas were ordered from multiple restaurants
+     */
+    PIZZA_FROM_MULTIPLE_RESTAURANTS,
+
+    /**
+     * the restaurant is closed on the order day
+     */
+    RESTAURANT_CLOSED
+}
