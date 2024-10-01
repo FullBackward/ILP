f955f5ab2989253b3481d12c8c70039304569be3
WiGHT <1572758387@qq.com>
2024-10-01 14:23:23 +0100

    feat: add ray-casting; used ray-casting for contain check, not tested

--- src/main/java/uk/ac/ed/inf/utils/LngLatHandler.java
+++ src/main/java/uk/ac/ed/inf/utils/LngLatHandler.java
@@ -1,11 +1,12 @@
 package uk.ac.ed.inf.utils;
+
+import uk.ac.ed.inf.constant.SystemConstants;
 import uk.ac.ed.inf.data.LngLat;
 import uk.ac.ed.inf.data.NamedRegion;
 
-import uk.ac.ed.inf.constant.SystemConstants;
-
 import java.math.BigDecimal;
 import java.math.MathContext;
+
 
 /*
 Helper functions to make App.class clean.
@@ -16,17 +17,20 @@ public class LngLatHandler implements uk.ac.ed.inf.interfaces.LngLatHandling {
      */
     @Override
     public double distanceTo(LngLat startPosition, LngLat otherPosition){
-        
-        BigDecimal lng1 = BigDecimal.valueOf(startPosition.lng());
-        BigDecimal lng2 = BigDecimal.valueOf(otherPosition.lng());
-        BigDecimal lat1 = BigDecimal.valueOf(startPosition.lat());
-        BigDecimal lat2 = BigDecimal.valueOf(otherPosition.lat());
-        BigDecimal ac = lng1.subtract(lng2).abs();
-        BigDecimal cb = lat1.subtract(lat2).abs();
-        //System.out.println("lng1: " + startPosition.lng() + "lng2: " + otherPosition.lng());
-        //System.out.println("lat1: " + startPosition.lat() + "lat2: " + otherPosition.lat());
-        //System.out.println("ac: " + ac + "cb: " + cb);
-        return ac.pow(2).add(cb.pow(2)).sqrt(MathContext.DECIMAL64).doubleValue();
+        try{
+            BigDecimal lng1 = BigDecimal.valueOf(startPosition.lng());
+            BigDecimal lng2 = BigDecimal.valueOf(otherPosition.lng());
+            BigDecimal lat1 = BigDecimal.valueOf(startPosition.lat());
+            BigDecimal lat2 = BigDecimal.valueOf(otherPosition.lat());
+            BigDecimal ac = lng1.subtract(lng2).abs();
+            BigDecimal cb = lat1.subtract(lat2).abs();
+            //System.out.println("lng1: " + startPosition.lng() + "lng2: " + otherPosition.lng());
+            //System.out.println("lat1: " + startPosition.lat() + "lat2: " + otherPosition.lat());
+            //System.out.println("ac: " + ac + "cb: " + cb);
+            return ac.pow(2).add(cb.pow(2)).sqrt(MathContext.DECIMAL64).doubleValue();
+        }catch(Exception exp){
+            return -1;
+        }
     }
 
     @Override
@@ -42,15 +46,27 @@ public class LngLatHandler implements uk.ac.ed.inf.interfaces.LngLatHandling {
         BigDecimal plng = BigDecimal.valueOf(position.lng());
         BigDecimal plat = BigDecimal.valueOf(position.lat());
         LngLat[] regionPoints = region.vertices();
-        BigDecimal a = BigDecimal.valueOf(0);
-        for(LngLat i : regionPoints){
-
+        boolean inside = false;
+        int len = regionPoints.length;
+        for (int i = 0; i < len; i++) {
+            if (RayCasting.intersects(
+                    new BigDecimal[] {BigDecimal.valueOf(regionPoints[i].lng()),
+                            BigDecimal.valueOf(regionPoints[i].lat())},
+                    new BigDecimal[] {BigDecimal.valueOf(regionPoints[(i + 1) % len].lng()),
+                            BigDecimal.valueOf(regionPoints[(i + 1) % len].lat())},
+                    new BigDecimal[]{plng, plat}))
+                inside = !inside;
         }
-        return false;
+        return inside;
     }
 
     @Override
     public LngLat nextPosition(LngLat startPosition, double angle) {
-        return null;
+        BigDecimal sinValue = BigDecimal.valueOf(Math.sin(Math.toRadians(angle)));
+        BigDecimal cosValue = BigDecimal.valueOf(Math.cos(Math.toRadians(angle)));
+        BigDecimal slng = BigDecimal.valueOf(startPosition.lng());
+        BigDecimal slat = BigDecimal.valueOf(startPosition.lat());
+        return new LngLat(slng.add(cosValue.multiply(BigDecimal.valueOf(SystemConstants.DRONE_MOVE_DISTANCE))).doubleValue()
+                , slat.add(sinValue.multiply(BigDecimal.valueOf(SystemConstants.DRONE_MOVE_DISTANCE))).doubleValue());
     }
 }
---
+++ src/main/java/uk/ac/ed/inf/utils/RayCasting.java
@@ -0,0 +1,31 @@
+package uk.ac.ed.inf.utils;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+//reference: https://rosettacode.org/wiki/Ray-casting_algorithm#Java
+//modified to BigDecimal
+public class RayCasting {
+    static boolean intersects(BigDecimal[] A, BigDecimal[] B, BigDecimal[] P) {
+        if (A[1].compareTo(B[1]) > 0)
+            return intersects(B, A, P);
+
+        if (P[1].compareTo(A[1]) == 0 || P[1].compareTo(B[1]) == 0)
+            P[1] = P[1].add(BigDecimal.valueOf(0.0001));
+
+        if (P[1].compareTo(B[1]) > 0 || P[1].compareTo(A[1]) < 0
+                || P[0].compareTo(BigDecimal.valueOf(Math.max(A[0].doubleValue(), B[0].doubleValue()))) >= 0)
+            return false;
+
+        if (P[0].compareTo(BigDecimal.valueOf(Math.min(A[0].doubleValue(), B[0].doubleValue()))) < 0)
+            return true;
+        try{
+            BigDecimal red = P[1].subtract(A[1]).divide(P[0].subtract(A[0]), MathContext.DECIMAL64);
+            BigDecimal blue = B[1].subtract(A[1]).divide(B[0].subtract(A[0]), MathContext.DECIMAL64);
+            return red.compareTo(blue) >= 0;
+        }catch(ArithmeticException exp){
+            System.out.println("[ERROR] SOURCE = RayCasting;" + exp);
+            return false;
+        }
+    }
+}
